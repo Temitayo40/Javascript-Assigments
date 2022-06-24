29480df486f9598c08cdda71cdf65e60c0d9f973
Adebowale <adebowaletemitayo40@gmail.com>
2022-06-24 22:24:52 +0100

    forgot to add the calc assignment

--- first/details.js
+++ first/details.js
@@ -1,3 +1,5 @@
+// this is the detail assignment.
+
 var full_name = "Adebowale Damilola";
 var height = "6ft" + " tall";
 var country = "Nigerian";
@@ -5,3 +7,22 @@ var country = "Nigerian";
 console.log(full_name);
 console.log(height);
 console.log(country);
+
+//please ignore this, this is meant for the calculator assignment
+
+var firstDigit;
+var secDigit;
+
+var question1 = prompt("Choose your math Operaions:\n" + "+ - / * ");
+firstDigit = prompt("Input your first number");
+secDigit = prompt("input your second number");
+
+if (question1 == "+") {
+  document.write(parseInt(firstDigit) + parseInt(secDigit));
+} else if (question1 == "-") {
+  document.write(parseInt(firstDigit) - parseInt(secDigit));
+} else if (question1 == "/") {
+  document.write(parseInt(firstDigit) / parseInt(secDigit));
+} else {
+  document.write(parseInt(firstDigit) * parseInt(secDigit));
+}
